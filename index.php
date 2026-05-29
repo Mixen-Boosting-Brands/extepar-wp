cac17f845bd09f589e63c88b7034bf5c4d61709e
Luis Carlos Pando <hey@luiscarlospando.com>
2026-05-29 11:17:39 -0600

    Fix animaciones

--- index.php
+++ index.php
@@ -311,7 +311,7 @@
                 </p>
             </div>
         </div>
-        <div class="row" data-aos="fade-up" data-aos-delay="80">
+        <div class="row" data-aos="zoom-in-up" data-aos-delay="80" data-aos-duration="1000">
             <div class="col-12">
                 <div class="services-section__img-wrapper">
                     <!-- Imagen: tabla "Planeación de ejecución en campo" -->
