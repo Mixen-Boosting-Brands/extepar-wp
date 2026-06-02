dcdbfabee7afbbb3dbfbbc12b8a5dcb4e963a4d1
Luis Carlos Pando <hey@luiscarlospando.com>
2026-06-02 17:25:26 -0600

    Cambios cliente

--- page-contacto-cv.php
+++ page-contacto-cv.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Contacto
+Template Name: Contacto CV
 */
 get_header(); ?>
 
@@ -27,7 +27,7 @@ get_header(); ?>
     </div>
 </section>
 
-<?php get_template_part("template-parts/formulario-a"); ?>
+<?php get_template_part("template-parts/formulario-c"); ?>
 
 <!-- ══════════════════════════════════════════════════════════
     PROYECTOS DONDE PODEMOS APORTAR VALOR
